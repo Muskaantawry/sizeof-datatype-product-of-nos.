ef5370a148bbaf6ece09689e9c0f2d80df79e0e3
Muskaantawry <65067255+Muskaantawry@users.noreply.github.com>
2020-06-05 12:54:45 +0530

    size od datatypes and product of numbers

---
+++ product of numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {
+ 	float a,b,c;
+ 	cout<<"enter the numbers to multiply \n";
+ 	cin>>a>>b;
+ 	c= a*b;
+ 	cout <<" the product of a & b is : "<< c;
+ 	return 0;
+ }
---
+++ sizeof datatypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	char ch; 
+	float a;
+	double b;
+	cout <<"size of int:"<< sizeof(i) <<"bytes\n";
+	cout <<"size of char:"<< sizeof(ch) <<"bytes\n";
+	cout <<"size of float :"<< sizeof(a) <<"bytes\n";
+	cout <<"size of double:"<< sizeof(b) <<"bytes \n";
+	return 0;
+}
